999b93793eb2dd547e76a2ce1f63a54b6ffdc2d2
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-04-21 17:13:43 +0800

    add stegsolve

---
+++ stegsolve/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#   Version 
+#   Author: WildfootW
+#   GitHub: github.com/Wildfoot
+#   Copyright (C) 2018 WildfootW All rights reserved.
+#
+
+wget http://www.caesum.com/handbook/Stegsolve.jar -O stegsolve.jar
+chmod +x stegsolve.jar
+mv stegsolve.jar ../bin/
+
