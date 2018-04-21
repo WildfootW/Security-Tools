22074fc0070d786ceaf2ed9e9f4b1a6b5065dc95
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-04-21 19:53:39 +0800

    add xortool

---
+++ xortool/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#   Version 
+#   Author: WildfootW
+#   GitHub: github.com/Wildfoot
+#   Copyright (C) 2018 WildfootW All rights reserved.
+#
+
+git clone https://github.com/hellman/xortool.git
+ln -s ../xortool/xortool/xortool/xortool ../bin/
+ln -s ../xortool/xortool/xortool/xortool-xor ../bin/
