e59ef468041a12d007e08d84513e4a9459d372c8
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-04-29 13:15:17 +0800

    add one gadget

---
+++ one_gadget/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#   Version 
+#   Author: WildfootW
+#   GitHub: github.com/Wildfoot
+#   Copyright (C) 2018 WildfootW All rights reserved.
+#
+
+sudo apt-get install ruby-dev
+gem install one_gadget
+
