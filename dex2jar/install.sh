3cd8fc619696bf9bceda5bf833b04e1d69107b6d
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-04-29 16:31:08 +0800

    add dex2jar

---
+++ dex2jar/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#   Version 
+#   Author: WildfootW
+#   GitHub: github.com/Wildfoot
+#   Copyright (C) 2018 WildfootW All rights reserved.
+#
+
+git clone https://github.com/pxb1988/dex2jar.git
