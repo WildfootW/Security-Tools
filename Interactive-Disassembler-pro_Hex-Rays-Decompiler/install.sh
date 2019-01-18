9065dc80d95dba4f05c0542fd59c982bd77906bb
WildfootW <wild.foot.yee.tzwu@gmail.com>
2019-01-18 10:46:51 +0800

    IDA: update install.sh

--- Interactive-Disassembler-pro_Hex-Rays-Decompiler/install.sh
+++ Interactive-Disassembler-pro_Hex-Rays-Decompiler/install.sh
@@ -21,8 +21,8 @@ BIN_folder="$SCRIPTPATH/../bin/"
 chmod 755 "$IDA_folder/idaq.exe"
 chmod 755 "$IDA_folder/idaq64.exe"
 
-echo "$IDA_folder/idaq.exe" > "$BIN_folder/ida-x86"
-echo "$IDA_folder/idaq64.exe" > "$BIN_folder/ida-x64"
+echo "wine $IDA_folder/idaq.exe" > "$BIN_folder/ida-x86"
+echo "wine $IDA_folder/idaq64.exe" > "$BIN_folder/ida-x64"
 
 chmod 755 "$BIN_folder/ida-x86"
 chmod 755 "$BIN_folder/ida-x64"
