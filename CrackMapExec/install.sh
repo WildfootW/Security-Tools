106a6d52ff13e46e3b358c3460a3eae32a627dbe
WildfootW <wildfootw@wildfoo.tw>
2020-07-23 17:04:28 +0800

    update CrackMapExec

--- CrackMapExec/install.sh
+++ CrackMapExec/install.sh
@@ -7,4 +7,7 @@
 
 apt-get install -y libssl-dev libffi-dev python-dev build-essential
 git clone --recursive https://github.com/byt3bl33d3r/CrackMapExec
-python3 ./CrackMapExec/setup.py install
+#cd CrackMapExec && \
+#    pipenv install && \
+#    pipenv shell && \
+#python setup.py install
