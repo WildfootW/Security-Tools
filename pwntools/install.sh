7ac753f16fa39f9472abc6100cf4c74839312d27
WildfootW <wildfootw@wildfoo.tw>
2020-07-27 21:22:09 +0800

    update pwntools

--- pwntools/install.sh
+++ pwntools/install.sh
@@ -7,4 +7,4 @@
 
 sudo apt-get update
 sudo apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
-python3 -m pip install --user --upgrade git+https://github.com/Gallopsled/pwntools.git@dev3
+python3 -m pip install --user --upgrade git+https://github.com/Gallopsled/pwntools.git@stable
